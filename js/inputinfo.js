78d4b46788896327c4751ecd54b92d3c6fc5e4df
James Clevenger <jeclevenger@caslmedia.com>
2015-03-27 00:28:50 -0500

    js files

---
+++ js/inputinfo.js
@@ -0,0 +1,300 @@
+var test1 = "Growing up in a family owned business..."
+var info = [
+        {
+	        "title": "Family Owned Resturant & Resort",
+	        "type": "employment",
+	        "start": "01 June 2000",
+	        "end": "15 August 2003",
+            "set": 5,
+	        "description": test1
+        },
+        {
+	        "title": "Family Owned Resturant & Resort",
+	        "type": "employment",
+	        "start": "15 May 2004",
+	        "end": "15 August 2004",
+            "set": 5,
+	        "description": test1
+        },
+        {
+	        "title": "Family Owned Resturant & Resort",
+			"type": "employment",
+	        "start": "15 May 2005",
+	        "end": "15 August 2005",
+            "set": 5,
+	        "description": test1
+        },
+        {
+	        "title": "Family Owned Resturant & Resort",
+	        "type": "employment",
+	        "start": "15 May 2006",
+	        "end": "15 August 2006",
+            "set": 5,
+	        "description": test1
+        },
+        {
+	        "title": "Family Owned Resturant & Resort",
+	        "type": "employment",
+	        "start": "15 May 2007",
+	        "end": "15 August 2007",
+            "set": 5,
+	        "description": test1
+        },
+        {
+	        "title": "Family Owned Resturant & Resort",
+	        "type": "employment",
+	        "start": "15 May 2008",
+	        "end": "15 August 2008",
+            "set": 5,
+	        "description": test1
+        },
+        {
+	        "title": "Super Market",
+			"type": "employment",
+	        "start": "15 May 2005",
+	        "end": "15 June 2006",
+            "set": 4,
+	        "description": "Standard Shelf-Stocker, Box-Boy positions"
+        },
+        {
+	        "title": "Hardware Store",
+			"type": "employment",
+	        "start": "01 August 2005",
+	        "end": "18 December 2008",
+            "set": 3,
+	        "description": "Standard Shelf-Stocker, Box-Boy positions, eventually moving to Customer Support and Satisfaction in both electrical and paint divsions."
+        },
+        {
+	        "title": "Self Employed Odd Jobs",
+			"type": "employment",
+	        "start": "01 June 2006",
+	        "end": "15 August 2006",
+            "set": 4,
+	        "description": "Filling vairous positions throughout the year, primarily in summer. Painting, contracted construction, lawn mowing, etc."
+        },
+        {
+	        "title": "Self Employed Odd Jobs",
+			"type": "employment",
+	        "start": "01 June 2007",
+	        "end": "15 August 2007",
+            "set": 4,
+	        "description": "Filling vairous positions throughout the year, primarily in summer. Painting, contracted construction, lawn mowing, etc."
+        },
+        {
+	        "title": "Self Employed Odd Jobs",
+			"type": "employment",
+	        "start": "01 June 2008",
+	        "end": "15 August 2008",
+            "set": 4,
+	        "description": "Filling vairous positions throughout the year, primarily in summer. Painting, contracted construction, lawn mowing, etc."
+        },
+        {
+	        "title": "Apartment Maintence",
+			"type": "employment",
+	        "start": "01 May 2009",
+	        "end": "15 July 2010",
+            "set": 3,
+	        "description": "Painting, moving furniture, landscaping, cleaning, etc"
+        },
+        {
+	        "title": "University Tutor",
+			"type": "employment",
+	        "start": "01 September 2010",
+	        "end": "15 February 2011",
+            "set": 3,
+	        "description": "Tutor engineering students in Statics, Dynamics, Mechanics of Materials, and Thermodynamics"
+        },
+        {
+	        "title": "R&D Machine Shop",
+			"type": "employment",
+	        "start": "15 September 2010",
+	        "end": "01 May 2012",
+            "set": 2,
+	        "description": "Intern, Co-op, Full-time Machinist. Filled various roles in a small team for a contract-based research and development of machining and cutting technologies. Proficient on 3 and 4 axis manual/CNC lathes and mills (conversational code, G-Code written or generated), trained on all standard shop equipment and various testing apparatuses (fluid and solid mechanics)."
+        },
+        {
+	        "title": "Industry Leading Engine Manufacturer (R&D)",
+			"type": "employment",
+	        "start": "15 May 2012",
+	        "end": "18 December 2012",
+            "set": 2,
+	        "description": "1-D 3-D modeling for engine system optimzation, etc, fix later, etsting too"
+        },
+        {
+	        "title": "Industry Leading Engine Manufacturer (R&D)",
+			"type": "employment",
+	        "start": "01 June 2013",
+	        "end": "Current",
+            "set": 2,
+	        "description": "Computational Fluid Dynamics Analyst, internal and external flow, engine optimization, validation with analysis, etc. fix later"
+        },
+        {
+	        "title": "Casl Media LLC",
+			"type": "employment",
+	        "start": "30 July 2014",
+	        "end": "Current",
+            "set": 1,
+	        "description": "Co-founder and CEO"
+        },
+        {
+	        "title": "Minnesota Public School",
+			"type": "formal education",
+	        "start": "15 August 1995",
+	        "end": "01 June 1996",
+            "set": 6,
+	        "description": "Kindergarten through first grade in the Land of 10,000 Lakes"
+        },
+        {
+	        "title": "Minnesota Public School",
+			"type": "formal education",
+	        "start": "15 August 1996",
+	        "end": "01 June 1997",
+            "set": 6,
+	        "description": "Kindergarten through first grade in the Land of 10,000 Lakes"
+        },
+        {
+	        "title": "Minnesota Private School",
+			"type": "formal education",
+	        "start": "15 August 1997",
+	        "end": "01 June 1998",
+            "set": 6,
+	        "description": "Second grade in a Private Catholic School"
+        },
+        {
+	        "title": "Michigan Public School",
+			"type": "formal education",
+	        "start": "15 August 1998",
+	        "end": "01 June 1999",
+            "set": 6,
+	        "description": "Third and forth grade in the Lower Penninsula, Fifth grade through Highschool in the Upper Penninsula"
+        },
+        {
+	        "title": "Michigan Public School",
+			"type": "formal education",
+	        "start": "15 August 1999",
+	        "end": "01 June 2000",
+            "set": 6,
+	        "description": "Third and forth grade in the Lower Penninsula, Fifth grade through Highschool in the Upper Penninsula"
+        },
+        {
+	        "title": "Michigan Public School",
+			"type": "formal education",
+	        "start": "15 August 2000",
+	        "end": "01 June 2001",
+            "set": 6,
+	        "description": "Third and forth grade in the Lower Penninsula, Fifth grade through Highschool in the Upper Penninsula"
+        },
+        {
+	        "title": "Michigan Public School",
+			"type": "formal education",
+	        "start": "15 August 2001",
+	        "end": "01 June 2002",
+            "set": 6,
+	        "description": "Third and forth grade in the Lower Penninsula, Fifth grade through Highschool in the Upper Penninsula"
+        },
+        {
+	        "title": "Michigan Public School",
+			"type": "formal education",
+	        "start": "15 August 2002",
+	        "end": "01 June 2003",
+            "set": 6,
+	        "description": "Third and forth grade in the Lower Penninsula, Fifth grade through Highschool in the Upper Penninsula"
+        },
+        {
+	        "title": "Michigan Public School",
+			"type": "formal education",
+	        "start": "15 August 2003",
+	        "end": "01 June 2004",
+            "set": 6,
+	        "description": "Third and forth grade in the Lower Penninsula, Fifth grade through Highschool in the Upper Penninsula"
+        },
+        {
+	        "title": "Michigan Public School",
+			"type": "formal education",
+	        "start": "15 August 2004",
+	        "end": "01 June 2005",
+            "set": 6,
+	        "description": "Third and forth grade in the Lower Penninsula, Fifth grade through Highschool in the Upper Penninsula"
+        },
+        {
+	        "title": "Michigan Public School",
+			"type": "formal education",
+	        "start": "15 August 2005",
+	        "end": "01 June 2006",
+            "set": 6,
+	        "description": "Third and forth grade in the Lower Penninsula, Fifth grade through Highschool in the Upper Penninsula"
+        },
+        {
+	        "title": "Michigan Public School",
+			"type": "formal education",
+	        "start": "15 August 2006",
+	        "end": "01 June 2007",
+            "set": 6,
+	        "description": "Third and forth grade in the Lower Penninsula, Fifth grade through Highschool in the Upper Penninsula"
+        },
+        {
+	        "title": "Michigan Public School",
+			"type": "formal education",
+	        "start": "15 August 2007",
+	        "end": "01 June 2008",
+            "set": 6,
+	        "description": "Third and forth grade in the Lower Penninsula, Fifth grade through Highschool in the Upper Penninsula"
+        },
+        {
+	        "title": "Michigan Public University",
+			"type": "formal education",
+	        "start": "25 August 2008",
+	        "end": "05 May 2010",
+            "set": 6,
+	        "description": "BSME BS in Mechanical Engineering with a Minor in Mathematics (Magna Cum Laude). Various affilliations including Formula SAE (engine and composites), Four Wheelers, SAE, ASME."
+        },
+        {
+	        "title": "Michigan Public University",
+			"type": "formal education",
+	        "start": "01 September 2010",
+	        "end": "05 May 2012",
+            "set": 6,
+	        "description": "BSME BS in Mechanical Engineering with a Minor in Mathematics (Magna Cum Laude). Various affilliations including Formula SAE (engine and composites), Four Wheelers, SAE, ASME."
+        },
+        {
+	        "title": "Michigan Public University",
+			"type": "formal education",
+	        "start": "10 January 2013",
+	        "end": "05 May 2013",
+            "set": 6,
+	        "description": "BSME BS in Mechanical Engineering with a Minor in Mathematics (Magna Cum Laude). Various affilliations including Formula SAE (engine and composites), Four Wheelers, SAE, ASME."
+        },
+        {
+	        "title": "Cumbria University (United Kingdom)",
+			"type": "formal education",
+	        "start": "03 July 2010",
+	        "end": "20 August 2010",
+            "set": 6,
+	        "description": "Study abroad in North-East England"
+        },
+        {
+	        "title": "Parental Home Teachings",
+			"type": "informal education",
+	        "start": "15 March 1990",
+	        "end": "25 August 2008",
+	        "type": "education",
+            "set": 8,
+	        "description": "In the nature versus nurture debate recent findings consistently show the effect of nurture (i.e. enviroment) as much more significant than previsouly concluded. The effects of the enviroment you were raised in cannot be understated, reconginzing this is (in my opinion) a crucial step in understanding an individual's thoughts, beliefs, and achievements."
+        },
+        {
+	        "title": "Indirect Computer Learning",
+			"type": "informal education",
+	        "start": "01 July 2000",
+	        "end": "15 March 2013",
+            "set": 7,
+	        "description": "Our first home computer. Originally purchased with the intent of managing the family business but quickly turned into my means to connect with the internet from our extremely rural location. Although I wasn't using the machine with intent of learning, indirectly I started to develop the ability to use it as a means to solve problems."
+        },
+        {
+	        "title": "Direct Computer Learning",
+			"type": "informal education",
+	        "start": "15 March 2013",
+	        "end": "Current",
+            "set": 7,
+	        "description": "From my first personal computer to the multitude of computing devices I use on a daily basis. Learning the capabilities of the comptuer, resources on the internet, structure of the internet, open knowledge, MOOCS, pirating stuff, etc. update later."
+        }
+    ];
